0abfd3b1443993d78e031b74b4ca73e6116aa470
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2024-08-16 08:52:31 +0530

    I have learnt chapter 23 of which name is child props

--- 18-22 Topics/learning-fragments/src/App.jsx
+++ 18-22 Topics/learning-fragments/src/App.jsx
@@ -1,16 +1,17 @@
 import ErrorMessage from "./components/ErrorMessages";
 import FoodItems from "./components/FoodItems";
+import Container from "./components/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   let foodItems = ["Meat", "Green Vegetables", "Roti", "Salad", "Milk", "Ghee"];
   return (
-    <>
+    <Container>
       <h1 className="item-heading">Healthy Foods</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
-    </>
+    </Container>
   );
 }
 
