630462fcb8216b553e4c0fa942cdc36a90ba8407
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2024-08-30 21:48:04 +0530

    18-22 Topics

--- 18-22 Topics/learning-fragment/src/components/Container.jsx
+++ 18-22 Topics/learning-fragment/src/components/Container.jsx
@@ -1,7 +1,7 @@
 import styles from "./Container.module.css";
 
-function Container({ children }) {
+const Container = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
-}
+};
 
 export default Container;
